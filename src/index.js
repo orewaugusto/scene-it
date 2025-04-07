f443ba686e2afbb3b0c38178531a878ff7e1c2a9
Augusto <danielaugustolopes44@gmail.com>
2025-04-06 21:58:47 -0300

    feat: create dist directory

--- src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const express = require('express');
-const app = express();
-const PORT = '8080';
-app.use(express.json());
-app.listen(PORT, () => {
-    console.log(`running on ${PORT}.`);
-});
