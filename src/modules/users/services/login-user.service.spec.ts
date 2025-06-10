41f7a0e765fd2cfe0c800e39ddc7f25e475eec0f
pedrohenriquemop <madeirapedrohenrique1112@gmail.com>
2025-06-09 23:40:19 -0300

    fix tests

--- src/modules/users/services/login-user.service.spec.ts
+++ src/modules/users/services/login-user.service.spec.ts
@@ -1,23 +1,25 @@
 import { UserRepositoryInterface } from "../repositories/interfaces/user-repository.interface";
 import { UserRepositoryMock } from "../repositories/users.repository.mock";
+import { CreateUserService } from "./create-user.service";
+import { CreateUserServiceInterface } from "./interfaces/create-user-service.interface";
 import { LoginUserServiceInterface } from "./interfaces/login-user.service.interface";
 import { LoginUserService } from "./login-user.service";
 
 describe("loginUserService", () => {
   let userRepository: UserRepositoryInterface;
   let loginUserService: LoginUserServiceInterface;
-  // let createUserService: CreateUserServiceInterface;
+  let createUserService: CreateUserServiceInterface;
 
   beforeEach(async () => {
     userRepository = new UserRepositoryMock();
-    // createUserService = new CreateUserService(userRepository);
+    createUserService = new CreateUserService(userRepository);
     loginUserService = new LoginUserService(userRepository, "Lana");
     process.env.TOKEN_SECRET = "0101";
-    // let newUser = await createUserService.execute({
-    //   email: "johndoe@gmail.com",
-    //   password: "teste123456",
-    //   username: "John Doe",
-    // });
+    await createUserService.execute({
+      email: "johndoe@gmail.com",
+      password: "teste123456",
+      username: "John Doe",
+    });
   });
 
   it("should be able to login with a valid email and password", async () => {
