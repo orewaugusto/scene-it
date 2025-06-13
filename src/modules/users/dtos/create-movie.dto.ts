ff93c46f52e2b82b704231e3cc1b33f79d3f113f
Augusto <danielaugustolopes44@gmail.com>
2025-06-13 03:22:33 -0300

    feat: create movie service and integrate TMDB API

---
+++ src/modules/users/dtos/create-movie.dto.ts
@@ -0,0 +1,23 @@
+import { IsInt, IsNotEmpty, IsOptional, IsString, IsUrl, Min, MaxLength } from "class-validator"
+
+export class CreateMovieDTO {
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(255)
+  title!: string
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(1000)
+  description?: string
+
+  @IsNotEmpty()
+  @IsInt()
+  @Min(1888)
+  releaseYear!: number
+
+  @IsOptional()
+  @IsString()
+  @IsUrl()
+  posterUrl?: string
+}
---
+++ src/modules/users/repositories/interfaces/movie-repository.interface.ts
@@ -0,0 +1,8 @@
+import { Movie } from "@prisma/client"
+import { CreateMovieDTO } from "../../dtos/create-movie.dto"
+
+export interface MovieRepositoryInterface {
+  findById(id: string): Promise<Movie | null>
+  findByTitle(title: string): Promise<Movie[]>
+  createMovie(movieData: CreateMovieDTO): Promise<Movie>
+}
