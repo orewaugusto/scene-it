16d1a3bb82d1e11b9084bd4d6595f227d0bc308d
pedrohenriquemop <madeirapedrohenrique1112@gmail.com>
2025-06-13 18:23:23 -0300

    fix lint problems

--- src/modules/users/services/interfaces/tmdb.service.interface.ts
+++ src/modules/users/services/interfaces/tmdb.service.interface.ts
@@ -1,6 +1,6 @@
-import { CreateMovieDTO } from "../../dtos/create-movie.dto"
+import { CreateMovieDTO } from "../../dtos/create-movie.dto";
 
 export interface TMDBServiceInterface {
-  searchMoviesByTitle(title: string): Promise<CreateMovieDTO[]>
-  getMovieById(tmdbId: string): Promise<CreateMovieDTO | null>
+  searchMoviesByTitle(title: string): Promise<CreateMovieDTO[]>;
+  getMovieById(tmdbId: string): Promise<CreateMovieDTO | null>;
 }
--- src/modules/users/services/tmdb.service.ts
+++ src/modules/users/services/tmdb.service.ts
@@ -1,16 +1,16 @@
-import axios from "axios"
-import { TMDBServiceInterface } from "./interfaces/tmdb.service.interface"
-import { CreateMovieDTO } from "../dtos/create-movie.dto"
+import axios from "axios";
+import { TMDBServiceInterface } from "./interfaces/tmdb.service.interface";
+import { CreateMovieDTO } from "../dtos/create-movie.dto";
 
 export class TMDBService implements TMDBServiceInterface {
-  private readonly baseURL = "https://api.themoviedb.org/3"
-  private readonly apiKey: string
+  private readonly baseURL = "https://api.themoviedb.org/3";
+  private readonly apiKey: string;
 
   constructor() {
     if (!process.env.TMDB_API_KEY) {
-      throw new Error("TMDB_API_KEY not set in environment variables")
+      throw new Error("TMDB_API_KEY not set in environment variables");
     }
-    this.apiKey = process.env.TMDB_API_KEY
+    this.apiKey = process.env.TMDB_API_KEY;
   }
 
   async searchMoviesByTitle(title: string): Promise<CreateMovieDTO[]> {
@@ -20,9 +20,12 @@ export class TMDBService implements TMDBServiceInterface {
         query: title,
         language: "en-US",
       },
-    })
+    });
 
-    return response.data.results.map((movie: any) => this.transformToDTO(movie))
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return response.data.results.map((movie: any) =>
+      this.transformToDTO(movie),
+    );
   }
 
   async getMovieById(tmdbId: string): Promise<CreateMovieDTO | null> {
@@ -32,20 +35,21 @@ export class TMDBService implements TMDBServiceInterface {
           api_key: this.apiKey,
           language: "en-US",
         },
-      })
-      return this.transformToDTO(response.data)
+      });
+      return this.transformToDTO(response.data);
     } catch (error) {
       if (axios.isAxiosError(error) && error.response?.status === 404) {
-        return null
+        return null;
       }
-      throw error
+      throw error;
     }
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   private transformToDTO(movie: any): CreateMovieDTO {
     const releaseYear = movie.release_date
       ? parseInt(movie.release_date.slice(0, 4))
-      : new Date().getFullYear()
+      : new Date().getFullYear();
 
     return {
       title: movie.title,
@@ -54,6 +58,6 @@ export class TMDBService implements TMDBServiceInterface {
       posterUrl: movie.poster_path
         ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
         : undefined,
-    }
+    };
   }
 }
