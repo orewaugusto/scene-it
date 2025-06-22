50cb9d688f950bb39865881ad762331ddf521e6e
pedrohenriquemop <madeirapedrohenrique1112@gmail.com>
2025-06-22 12:51:18 -0300

    add pagination to find movies function (apparently does not work if the movies come from the tmdb api, see it later)

--- src/modules/movies/repositories/movies.repository.mock.ts
+++ src/modules/movies/repositories/movies.repository.mock.ts
@@ -1,23 +1,51 @@
 import { Movie } from "@prisma/client";
 import { CreateMovieDTO } from "../dtos/create-movie.dto";
-import { MoviesRepositoryInterface } from "./interfaces/movies-repository.interface";
+import {
+  MoviesRepositoryInterface,
+  PaginatedMovies,
+} from "./interfaces/movies-repository.interface";
 
 export class MoviesRepositoryMock implements MoviesRepositoryInterface {
   private db: Movie[] = [];
+  private currentId = 1;
 
-  async findMoviesByTitle(title: string): Promise<Movie[]> {
-    return this.db.filter((movie) =>
-      movie.title.toLowerCase().includes(title.toLowerCase())
-    );
+  async findMovies(
+    title: string | undefined,
+    limit: number,
+    offset: number,
+  ): Promise<PaginatedMovies> {
+    let filteredMovies = this.db;
+
+    if (title) {
+      filteredMovies = this.db.filter((movie) =>
+        movie.title.toLowerCase().includes(title.toLowerCase()),
+      );
+    }
+
+    const paginatedMovies = filteredMovies.slice(offset, offset + limit);
+
+    return { movies: paginatedMovies, totalCount: filteredMovies.length };
   }
 
   async createMovie(movieData: CreateMovieDTO): Promise<Movie> {
+    const existingMovie = this.db.find(
+      (movie) =>
+        movie.title === movieData.title &&
+        movie.releaseYear === movieData.releaseYear,
+    );
+
+    if (existingMovie) {
+      throw new Error(
+        "Film already exists with the same title and release year.",
+      );
+    }
+
     const newMovie: Movie = {
-      id: this.db.length,
+      id: this.currentId++,
       title: movieData.title,
-      description: movieData.description!,
+      description: movieData.description || null,
       releaseYear: movieData.releaseYear,
-      posterUrl: movieData.posterUrl!,
+      posterUrl: movieData.posterUrl || null,
       createdAt: new Date(),
       updatedAt: new Date(),
     };
--- src/modules/movies/services/interfaces/find-movies-by-title-service.interface.ts
+++ src/modules/movies/services/interfaces/find-movies-by-title-service.interface.ts
@@ -1,5 +1,10 @@
 import { MovieEntity } from "../../repositories/interfaces/tmdb-repository.interface";
+import { PaginatedMovies } from "../../repositories/interfaces/movies-repository.interface"; // New import
 
 export interface FindMoviesByTitleServiceInterface {
-  execute(title: string): Promise<MovieEntity[]>;
+  execute(
+    title: string | undefined,
+    page: number,
+    limit: number,
+  ): Promise<PaginatedMovies | MovieEntity[]>;
 }
